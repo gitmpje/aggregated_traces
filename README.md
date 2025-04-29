6db1daec55a476e74823d7a2e5a60ec2c9df90f8
Mark van der Pas <mark.van.der.pas@semaku.com>
2025-04-29 13:30:23 +0200

    Add option to use Oxigraph and optimize SPARQL queries

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 # Aggregated Traces
 
-
 ## Dependencies
-Note that `pygraphviz` requires Graphviz to be installed and discoverable. For installation instructions check https://pygraphviz.github.io/documentation/stable/install.html.
+Note that `pygraphviz` requires Graphviz to be installed and discoverable. For installation instructions check [PyGraphviz - Install](https://pygraphviz.github.io/documentation/stable/install.html).
+
+## Get started
+Create a virtual environment.
+```
+python -m venv .venv
+```
+
+Activate virtual environment.
+```
+source .venv/Scripts/activate
+```
+
+Install dependencies. Make sure that Graphviz is installed and discoverable.
+```
+python -m pip install -e .
+```
 
 ## Method
 ### Mapping from traceability graph model to Event Knowledge Graph
@@ -15,3 +30,9 @@ Note that `pygraphviz` requires Graphviz to be installed and discoverable. For i
 |                       |    `Transformation`    | source - `outputQuantity` |  target - `childQuantity`* |   source - `outputQuantity`    | target - `inputQuantity` |
 
 *Assumes that all quantities that are 'merged' are included in the child quantities.
+
+## Tests
+
+```
+python -m pytest
+```
--- aggregated_traces/utils/construct_ekg.py
+++ aggregated_traces/utils/construct_ekg.py
@@ -2,38 +2,101 @@ import logging
 
 from pathlib import Path
 from networkx import MultiDiGraph
-from rdflib import ConjunctiveGraph, Graph, Variable
+from rdflib import Dataset, Graph, URIRef, Variable
 from rdflib.plugins.sparql.processor import SPARQLResult
 from time import time
-from typing import Dict
+from typing import Dict, Optional
 
 path_queries = Path(__file__).parent.parent.joinpath("sparql")
 
-logging.addLevelName(logging.INFO+1, "INFO (timing)")
+logging.addLevelName(logging.INFO + 1, "INFO (timing)")
 logger = logging.getLogger(__name__)
 
 
-def load_rdf_graph(file: str|Path) -> ConjunctiveGraph:
+RELATION_GRAPH_ID = [
+    (
+        "DF",
+        "http://example.org/def/ekg/aggregated_traces/DirectlyFollows_AggregatedEntity",
+        "urn:ekg:directlyFollows",
+    ),
+    (
+        "DP",
+        "http://example.org/def/ekg/aggregated_traces/DirectlyPrecedes_AggregatedEntity",
+        "urn:ekg:directlyPrecedes",
+    ),
+]
+
+
+def load_rdf_graph(file: str | Path, store: Optional[str] = "Memory") -> Dataset:
     start_time = time()
 
-    graph = ConjunctiveGraph()
+    graph = Dataset(default_union=True, store=store)
     graph.parse(file)
 
-    logger.log(logging.INFO+1, "load_rdf_graph: %.2f s", time() - start_time)
+    logger.log(logging.INFO + 1, "load_rdf_graph: %.2f s", time() - start_time)
 
     return graph
 
 
 def insert_DF_DP(g: Graph) -> Graph:
     start_time = time()
-    with open(path_queries.joinpath("insert_DF+DP_aggregated_entity.ru")) as f:
-        g.update(f.read())
-    logger.log(logging.INFO+1, "Insert_DF_DP - direct relation: %.2f s", time() - start_time)
 
-    start_time = time()
-    with open(path_queries.joinpath("insert_DF+DP_qualified_relation.ru")) as f:
-        g.update(f.read())
-    logger.log(logging.INFO+1, "Insert_DF_DP - qualified relation: %.2f s", time() - start_time)
+    # For Oxigraph construct graphs instead of update, as updates are not handled by pyoxigraph (see README on https://github.com/oxigraph/oxrdflib)
+    if "Oxigraph" in str(g.store):
+        with open(path_queries.joinpath("DF+DP_construct_temp.rq")) as f:
+            g_temp = g.query(f.read())
+        g += g_temp
+
+        # Direct relations
+        for relation_name, relation, graph_id in RELATION_GRAPH_ID:
+            with open(
+                path_queries.joinpath(f"DF+DP_construct_{relation_name}.rq")
+            ) as f:
+                r = g.query(f.read())
+            g_r = Graph(identifier=graph_id)
+            g_r.parse(data=r.graph.serialize())
+            g += g_r
+
+        logger.log(
+            logging.INFO + 1,
+            "Insert_DF_DP - direct relation: %.2f s",
+            time() - start_time,
+        )
+
+        # Qualified relations
+        for relation_name, relation, graph_id in RELATION_GRAPH_ID:
+            with open(
+                path_queries.joinpath(
+                    f"DF+DP_construct_{relation_name}_qualified_relation.rq"
+                )
+            ) as f:
+                r = g.query(f.read())
+            g_r = Graph(identifier=graph_id)
+            g_r.parse(data=r.graph.serialize())
+            g += g_r
+
+        logger.log(
+            logging.INFO + 1,
+            "Insert_DF_DP - qualified relation: %.2f s",
+            time() - start_time,
+        )
+    else:
+        with open(path_queries.joinpath("insert_DF+DP_aggregated_entity.ru")) as f:
+            g.update(f.read())
+        logger.log(
+            logging.INFO + 1,
+            "Insert_DF_DP - direct relation: %.2f s",
+            time() - start_time,
+        )
+
+        start_time = time()
+        with open(path_queries.joinpath("insert_DF+DP_qualified_relation.ru")) as f:
+            g.update(f.read())
+        logger.log(
+            logging.INFO + 1,
+            "Insert_DF_DP - qualified relation: %.2f s",
+            time() - start_time,
+        )
 
     return g
 
@@ -46,16 +109,45 @@ def check_quantities(g: Graph) -> SPARQLResult:
     if not all([b[Variable("equal")].toPython() for b in r.bindings]):
         logging.warning("Not all nodes have incoming amount equal to outgoing amount!")
 
-    logger.log(logging.INFO+1, "check_quantities: %.2f s", time() - start_time)
+    logger.log(logging.INFO + 1, "check_quantities: %.2f s", time() - start_time)
 
     return r
 
 
 def insert_fractions(g: Graph) -> Graph:
     start_time = time()
-    with open(path_queries.joinpath("insert_quantity_fraction.ru")) as f:
-        g.update(f.read())
-    logger.log(logging.INFO+1, "insert_fractions: %.2f s", time() - start_time)
+
+    # For Oxigraph construct graphs instead of update, as updates are not handled by pyoxigraph (see README on https://github.com/oxigraph/oxrdflib)
+    if "Oxigraph" in str(g.store):
+        # Relation amount out
+        with open(
+            path_queries.joinpath("quantity_fraction_construct_amount_out.rq")
+        ) as f:
+            g_temp = g.query(f.read())
+        g += g_temp
+
+        # Event sum amount out
+        with open(
+            path_queries.joinpath("quantity_fraction_construct_sum_amount_out.rq")
+        ) as f:
+            g_temp = g.query(f.read())
+        g += g_temp
+
+        # Fraction (per relation type)
+        for relation_name, relation, graph_id in RELATION_GRAPH_ID:
+            with open(
+                path_queries.joinpath("quantity_fraction_construct_fraction.rq")
+            ) as f:
+                r = g.query(f.read().replace("?type", f"<{relation}>"))
+            g_r = Graph(identifier=graph_id)
+            g_r.parse(data=r.graph.serialize())
+            g += g_r
+
+    else:
+        with open(path_queries.joinpath("insert_quantity_fraction.ru")) as f:
+            g.update(f.read())
+
+    logger.log(logging.INFO + 1, "insert_fractions: %.2f s", time() - start_time)
 
     return g
 
@@ -90,6 +182,8 @@ def generate_networkx_di_graph(g: Graph) -> MultiDiGraph:
             key=b[Variable("Relation")],
             **get_attributes(b, "edge"),
         )
-    logger.log(logging.INFO+1, "generate_networkx_di_graph: %.2f s", time() - start_time)
+    logger.log(
+        logging.INFO + 1, "generate_networkx_di_graph: %.2f s", time() - start_time
+    )
 
     return nx_graph
